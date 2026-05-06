3aa96f87140218ab357337a867ab074a3f38d864
Roderick Fanou <rfexplorer@proton.me>
2026-05-05 22:18:54 -0500

    Remove base path for rodricfanou.github.io

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/website",
-  assetPrefix: "/website/",
   images: {
     unoptimized: true,
   },
