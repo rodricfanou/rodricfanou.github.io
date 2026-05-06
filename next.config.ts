fe89872f4bf06b02ebe16027d82a310026d27fff
Roderick Fanou <rfexplorer@proton.me>
2026-05-05 21:43:59 -0500

    Update repo name and base path for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/MyPersonalWebsite",
+  basePath: "/website",
   images: {
     unoptimized: true,
   },
